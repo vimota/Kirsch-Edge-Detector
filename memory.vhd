c9b1e66f54ab62db1ece6dd863b950745413323e
Victor Mota <vimota@gmail.com>
2014-07-16 20:40:23 -0400

    Added counters to memory

--- memory.vhd
+++ memory.vhd
@@ -8,18 +8,29 @@ entity memory is
 		i_reset  : in std_logic;
 		i_pixel  : in std_logic_vector(7 downto 0);
 		i_clock  : in std_logic;
-		o_valid  : out std_logic;
-		o_mode   : out std_logic_vector(2 downto 0);
-		o_row    : out std_logic_vector(2 downto 0);
+		-- o_valid  : out std_logic;
+		-- o_mode   : out std_logic_vector(2 downto 0);
+		o_column : out std_logic_vector(7 downto 0);
+		o_row    : out std_logic_vector(7 downto 0);
 		type image_type is array (8 downto 0) of
-			std_logic_vector(7 downto 0) ;
-		o_image  : out image_type
+			std_logic_vector(7 downto 0) 
+		-- o_image  : out image_type
 		);
 end memory ;
 
 architecture main of memory is
+
+	-- A function to rotate left (rol) a vector by n bits
+	function "rol" ( a : std_logic_vector; n : natural )
+		return std_logic_vector
+	is
+	begin
+		return std_logic_vector( unsigned(a) rol n );
+	end function;
+
+
 	constant mem_data_width : natural := 8;
-	constant mem_addr_width : natural := 256;
+	constant mem_addr_width : natural := 8;
 	signal mem_data : std_logic_vector(mem_data_width - 1 downto 0) ;
 	signal mem_addr : std_logic_vector(mem_addr_width - 1 downto 0) ;
 	type mem_q_type is array (2 downto 0 ) of std_logic_vector(mem_data_width - 1 downto 0) ;
@@ -70,14 +81,14 @@ begin
 	);
 
 	-- writing process
-	process (i_clock)
-	begin
-		if rising_edge(i_clock) then
-			if (i_valid = '1') then
+	-- process (i_clock)
+	-- begin
+	-- 	if rising_edge(i_clock) then
+	-- 		if (i_valid = '1') then
 
-			end if;
-		end if;
-	end process;
+	-- 		end if;
+	-- 	end if;
+	-- end process;
 	
 	-- reading process
 	process (i_clock)
@@ -85,6 +96,9 @@ begin
 		if rising_edge(i_clock) then
 			if (i_valid = '1') then
 			--	update column
+
+
+
 				if (column = 255) then
 					column <= 0;
 					row <= row + 1;
@@ -93,11 +107,14 @@ begin
 					column <= column + 1;
 				end if;
 
+
+
 			end if;
 		end if;
 	end process;
 	
 	mem_addr <= std_logic_vector(column);
+	o_column <= mem_addr;
 	o_row <= std_logic_vector(row);
 
 end main;
